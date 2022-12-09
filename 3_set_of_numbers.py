f7c66fd5d0d451f951c01d9fe698f52eab250f4f
Татьяна <119119137+SuperBobaBiba@users.noreply.github.com>
2022-12-10 00:07:16 +0300

    Add files via upload

--- 3_set_of_numbers.py
+++ 3_set_of_numbers.py
@@ -10,28 +10,15 @@ print ('массив a:', a)
 print ('массив b:', b)
 
 #входят одновременно в оба
-two=set(a).intersection(set(b))
-print('входят одновременно в оба массива:',sorted(two))
+print('входят одновременно в оба массива:',sorted(set(a).intersection(set(b))))
 
 #входят только в первое, но не входят во второе
-a1=[]
-for i in range(0,N):
-    if a[i] not in b:
-        a1.append(a[i])
-print('входят только в первый массив:', sorted(set(a1)))
+a1=set(a)-set(b)
+print('входят только в первый массив:', sorted(a1))
 
 #входят только во второе, но не входят в первое;
-b1=[]
-for i in range(0,N):
-    if b[i] not in a:
-        b1.append(b[i])
-print('входят только во второй массив:', sorted(set(b1)))
+b1=set(b)-set(a)
+print('входят только во второй массив:', sorted(b1))
 
 #входят в первое или во второе, но не в оба из них одновременно.
-c1=[]
-for i in range(0,N):
-    if a[i] not in two:
-        c1.append(a[i])
-    if b[i] not in two:
-        c1.append(b[i])
-print('входят в первый или во второй, но не в оба из них одновременно:', sorted(set(c1)))
+print('входят в первый или во второй, но не в оба из них одновременно:', sorted(a1.union(b1)))
